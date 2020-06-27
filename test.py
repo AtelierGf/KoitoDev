d59729a705389dc38782710da38b62780093e561
User Senk <senk@www4174.sakura.ne.jp>
2020-06-27 10:56:56 +0900

    [ADD]test

--- test.py
+++ test.py
@@ -11,24 +11,22 @@ def main():
 
     twitter=OAuth1Session(CKey,CSKey,TKey,TSKey)
 
-    follower_url = "https://api.twitter.com/1.1/followers/ids.json"
-    user_tl_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    home_url="https://api.twitter.com/1.1/statuses/home_timeline.json"
     update_url = "https://api.twitter.com/1.1/statuses/update.json"
 
     dame="{}、わたしがいないとだめなんですよー！"
-    yoyu="い、いえ！{}はよゆーですよ\n全然平気です！"
-    template=[dame,yoyu]
+    #yoyu="い、いえ！{}はよゆーですよ\n全然平気です！"
+    yoyu="{}テスト2"
+    template=[dame,yoyu] 
 
-    t = MeCab.Tagger("-Ochasen -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
-    follower=twitter.get(follower_url)
+    t=MeCab.Tagger("-Ochasen")
+    #t=MeCab.Tagger("-Ochasen-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
 
-    ids=(json.loads(follower.text))["ids"]
-    i=random.randint(0,len(ids)-1)
-
-    timeline=twitter.get(user_tl_url,params={"user_id":i})
-    if timeline.status_code == 200:
-        tweets = ((json.loads(timeline.text)))
-        tweet = tweets[random.randint(0,len(tweets)-1)]
+    t.parse("")
+    res=twitter.get(home_url)
+    if res.status_code == 200:
+        timeline=(json.loads(res.text))
+        tweet = timeline[random.randint(0,len(timeline)-1)]
         m = t.parseToNode(tweet["text"])
         nouns = []
         while m:
@@ -36,11 +34,9 @@ def main():
                 nouns.append(m.surface)
             m = m.next
 
-        r1=random.randint(0,1)
+        r1=random.randint(1,1)
         r2=random.randint(0,len(nouns)-1)
         tweet={"status":template[r1].format(nouns[r2])}
-
-        print(tweet["status"])
 
         res=twitter.post(update_url,params=tweet)
         if res.status_code == 200:
