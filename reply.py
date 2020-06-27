0a3b8a339d034237d8f590314e04396f5ee07d2e
AtelierGf <revofal@gmail.com>
2020-06-27 17:35:23 +0900

    [ADD]

--- reply.py
+++ reply.py
@@ -1,4 +1,4 @@
-import json,config,random
+import json,config,random,os
 from requests_oauthlib import OAuth1Session
 
 def main():
@@ -7,25 +7,25 @@ def main():
     CKey=config.TW_CONSUMER_KEY.strip()
     CSKey=config.TW_CONSUMER_SECRET.strip()
     User=config.USER.strip()
-
     twitter=OAuth1Session(CKey,CSKey,TKey,TSKey)
 
+    log_path=os.getcwd()+"/reply_log.txt"
     reply_list=["ぴぇ！？","ぴゃ・・・！？","みんな、わたしがいないとだめなんですよー！"]
+    params={"count":5}
 
-#    with open(r"./reply_log.txt",mode="r") as f:
-#        temp=f.readline()
-#        since_id=temp if temp!="" else 1
+    with open(log_path,mode="r") as f:
+        line=f.readline()
+        id=int(line) if line!="" else 0
+        if id:
+            params["since_id"]=id
 
     mention_url='https://api.twitter.com/1.1/statuses/mentions_timeline.json' 
     update_url="https://api.twitter.com/1.1/statuses/update.json"
-    params={"count":5}
 
     res=twitter.get(mention_url,params=params)
     if res.status_code == 200:
         print("Success getting mention!")
         mentions=json.loads(res.text)
-
-        print(mentions)
         for mention in mentions:
             '''
             replying_tweet_id = tweet id replying now
@@ -34,28 +34,25 @@ def main():
             #get information to reply and record this id.
             target_tweet_id=mention["id"]
             user_name=mention["user"]["screen_name"]
-            with  open(r"./reply_log.txt",mode="w") as f:
-                print(target_tweet_id) 
-                f.write(str(target_tweet_id))
 
             #generate reply text
             r=random.randint(0,len(reply_list)-1)
             reply_text="@"+user_name+"\n"+reply_list[r]
             tweet={"status":reply_text,"in_reply_to_status_id":target_tweet_id}
+            print(tweet)
+
+            with open(log_path,mode="w") as f:
+                print(target_tweet_id)
+                f.write(str(target_tweet_id))
 
             #attempt a post request.
-            res=twitter.post(update_url,params=tweet)
-            if res.status_code == 200:
-                print("Success!")
-            else:
-                print("Failed : %d"% res.status_code)
+#            res=twitter.post(update_url,params=tweet)
+#            if res.status_code == 200:
+#                print("Success!")
+#            else:
+#                print("Failed : %d"% res.status_code)
     else:
         print("Failed. : %d"% res.status_code)
-
-        #determine reply to me.
-        #for user in mention["entities"]["user_mentions"]:
-            #if user["screen_name"] == "TrendKoito":
-                #mention_me=True
 
 if __name__=='__main__':
     main()
