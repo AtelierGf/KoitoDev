985a81929ac4016ee98218ba9fb01981ed80f0e4
AtelierGf <revofal@gmail.com>
2020-06-27 11:25:00 +0900

    [ADD]

---
+++ ng_word.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-                                                                                                                                          
+def filtering(tweets):
+         ng_words=["https","http","RT"]
+         for word in ng_words:
+                 tweets=list(filter(lambda x:word not in x,tweets))
+         return tweets
--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import MeCab 
 import json,config,random,re
 from requests_oauthlib import OAuth1Session
+import ng_word
 
 def main():
     CKey=config.TW_CONSUMER_KEY.strip()
@@ -15,14 +16,14 @@ def main():
     update_url = "https://api.twitter.com/1.1/statuses/update.json"
 
     dame="{}、わたしがいないとだめなんですよー！"
-    #yoyu="い、いえ！{}はよゆーですよ\n全然平気です！"
-    yoyu="{}テスト2"
+    yoyu="い、いえ！{}はよゆーですよ\n全然平気です！"
+    #yoyu="{}テスト2"
     template=[dame,yoyu] 
 
-    t=MeCab.Tagger("-Ochasen")
-    #t=MeCab.Tagger("-Ochasen-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-
+   # t=MeCab.Tagger("-Ochasen-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    t=MeCab.Tagger('-Ochasen -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd') 
     t.parse("")
+
     res=twitter.get(home_url)
     if res.status_code == 200:
         timeline=(json.loads(res.text))
@@ -34,18 +35,21 @@ def main():
                 nouns.append(m.surface)
             m = m.next
 
+        nouns=ng_word.filtering(nouns)
         r1=random.randint(1,1)
         r2=random.randint(0,len(nouns)-1)
         tweet={"status":template[r1].format(nouns[r2])}
 
-        res=twitter.post(update_url,params=tweet)
-        if res.status_code == 200:
-            print("Success!")
-        else:
-            print("Failed : %d"% res.status_code)
+        print(tweet["status"])
+
+#        res=twitter.post(update_url,params=tweet)
+#        if res.status_code == 200:
+#            print("Success!")
+#        else:
+#            print("Failed : %d"% res.status_code)
 
     else:
-        print("Failed: %d"% timeline.status_code)
+        print("Failed: %d"% res.status_code)
 
 if __name__=='__main__':
     main()
